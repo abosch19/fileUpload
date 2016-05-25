2101e8a41b7bfd8b391fe9e21b9476b1f596e64b
Alex Bosch <abosch19@gmail.com>
2016-05-25 21:32:52 +0200

    Tree structure done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require('express')
 	app = express()
+	folder_route = require('./routes/folder')
+	home_route = require('./routes/home.js')
 
-app.set('view engine', 'ejs')
 app.set('port', (process.env.PORT || 8080))
 
-app.use('static/', express.static(__dirname + '/public/'))
+// views is directory for all template files
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-	res.send('NODE JS')
+app.use('/static', express.static(__dirname + '/public')) //Static files
+
+//Routes
+app.use('/folder', folder_route)
+app.use('/', home_route)
+app.use('*', function (req, res) {
+	res.redirect('/')
 })
 
 app.listen(app.get('port'), function () {
---
+++ routes/folder.js
@@ -0,0 +1,26 @@
+var express = require('express')
+	router = express.Router()
+	multer = require('multer');
+	storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, '/files')
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.originalname)
+		}
+	})
+	upload = multer({ storage: storage })
+
+	router.get('/', function (req, res) {
+		res.send('Folder page')
+	})
+
+	router.post('/:folder', upload.array('files', 9), function (req, res) {
+		res.write('Files uploaded:')
+		for(file in req.files){
+			res.write(file)
+		}
+		res.end()
+	})
+
+module.exports = router
