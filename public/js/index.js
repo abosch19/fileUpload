90d51b55d84102c32ee0f6ff0f8c5880021e00f8
Alex Bosch <abosch19@gmail.com>
2016-05-27 02:03:49 +0200

    Ultims canvis

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-  $('.how_work').mouseover(function(){
-    $(this).css({cursor:"pointer"});
-  });
+    $('.how_work').mouseover(function(){
+        $(this).css({cursor:"pointer"});
+    });
 
-  $(".how_work, .nav_how").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".tutorial").offset().top
-    }, 1000);
-  });
+    $(".how_work, .nav_how").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".tutorial").offset().top
+        }, 1000);
+    });
 
-  $(window).scroll(function (){
-      $('section.home').css('top', $(window).scrollTop()/2);
-  });
+    $(window).scroll(function (){
+        $('section.home').css('top', $(window).scrollTop()/2);
+    });
+
+    $('.cross').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
 });
